f5de6504cab5538aa0d2409b6a454ccd49a3bf8a
VincentGörner <161941709+A1exi0sD4rc@users.noreply.github.com>
2025-01-16 20:17:00 +0100

    atf-section_navbar-c_created

--- src/app/above-the-fold/above-the-fold.component.ts
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-above-the-fold',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './above-the-fold.component.html',
   styleUrl: './above-the-fold.component.scss'
 })
