2dcd545ecc8450413fd3c65957849a526cc80485
VincentGörner <161941709+A1exi0sD4rc@users.noreply.github.com>
2025-02-01 16:42:06 +0100

    reference-section_updated

--- src/app/references/references.component.ts
+++ src/app/references/references.component.ts
@@ -12,15 +12,52 @@ export class ReferencesComponent {
   references = [
     {
       autor: 'A. Fischer - Frontend Developer',
-      feedback: 'Vincent has proven to be a reliable group partner. His technical skills and proactive approach were crucial to the success of our project.'
+      feedback: 'Vincent has proven to be a reliable group partner. His technical skills and proactive approach were crucial to the success of our project.',
+      highlighted: false
     },
     {
       autor: 'T. Schulz - Team Partner',
-      feedback: 'Our project benefited enormously from Vincent efficient way of working.'
+      feedback: 'Our project benefited enormously from Vincent efficient way of working.',
+      highlighted: false
     },
     {
       autor: 'J. Janisch - Team Partner',
-      feedback: 'Working with Vincent was really good and effectily. His knowledge is impressiv and his kind to think always useful.'
+      feedback: 'Working with Vincent was really good and effectily. His knowledge is impressiv and his kind to think always useful.',
+      highlighted: false
     }
   ];
+  isHighlighted: number = 1;
+
+  constructor() {
+    this.highlightFeedback();
+  }
+
+  highlightFeedback() {    
+    for (let index = 0; index < this.references.length -1; index++) {
+      if (index == this.isHighlighted) {
+        this.references[this.isHighlighted].highlighted = true;        
+      } else {
+        this.references[this.isHighlighted].highlighted = false;
+      }
+    }
+  }
+
+  setHighlight(next: string) {
+    if (next == '-') {
+      if (this.isHighlighted > 0) {
+        this.isHighlighted--;
+      } else {
+        this.isHighlighted = this.references.length - 1;
+      }
+    }
+    if (next == '+') {
+      if (this.isHighlighted < this.references.length - 1) {
+        this.isHighlighted++;
+      } else {
+        this.isHighlighted = 0;
+      }
+    }
+    this.highlightFeedback();
+    
+  }
 }
