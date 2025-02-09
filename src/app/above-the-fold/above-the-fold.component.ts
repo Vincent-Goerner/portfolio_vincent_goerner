dcf3696a0548dbc26a3a31cab1b8e37024921d13
VincentGörner <161941709+A1exi0sD4rc@users.noreply.github.com>
2025-02-09 17:18:05 +0100

    design_updated

--- src/app/above-the-fold/above-the-fold.component.ts
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -11,22 +11,7 @@ import { HeaderComponent } from '../shared/header/header.component';
 })
 
 export class AboveTheFoldComponent {
-  marqueSpeedWork: number = 0;
-  marqueSpeedContact: number = 0;
 
   constructor() {
-  }
-
-  startMarquee(btn: string) {
-    if (btn == 'work') {
-      this.marqueSpeedWork = 6;
-    } else {
-      this.marqueSpeedContact = 6;
-    }
-  }
-
-  stopMarquee() {
-    this.marqueSpeedContact = 0;
-    this.marqueSpeedWork = 0;
   }
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -10,10 +10,24 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   changeLanguage: Boolean = false;
   language: string = this.switchLanguage();
+  isScrolled = false;
+
+  constructor() {
+    setInterval(() => {
+      this.onWindowScrolled();
+    }, 100);    
+  }
 
   switchLanguage() {
     this.changeLanguage = !this.changeLanguage;
     return this.language = (this.changeLanguage) ? 'EN' : 'DE';
   }
 
+  onWindowScrolled() {
+    if (window.scrollY > 0) {
+      this.isScrolled = true;
+    } else {
+      this.isScrolled = false;
+    }
+  }
 }
